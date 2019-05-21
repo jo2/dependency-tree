2b02bad9cb6c0597884371404510279f2d1f9436
lorvog <Lorenz.Vossgaetter@adesso.de>
2019-05-21 10:42:07 +0200

    add dummy file in emptypackage, fixed InvalidDependencyTest and CoreTest
    added dummy file to emptypackage so its included with by git, fixed InvalidDependencyTest to match specification in regards to analyzing error free code only, fixed a refractoring mistake in CoreTest

--- src/test/resources/testSrc/org/wickedsource/dependencytree/test/CoreTest.java
+++ src/test/resources/testSrc/org/wickedsource/dependencytree/test/CoreTest.java
@@ -1,7 +1,7 @@
 //These imports shouldn't matter to the DependencyTree
 import java.util.Objects;
 import java.io.BufferedReader;
-import org.wickedsource.dependencytree.test.CoreDependency;
+import org.wickedsource.dependencytree.test.CoreDependencyTest;
 //Wildcard imports should add all classes contained in the package as dependencies
 import org.wickedsource.dependencytree.test.wildcardpackage.*;
 
--- src/test/resources/testSrc/org/wickedsource/dependencytree/test/somepackage/InvalidDependencyTest.java
+++ src/test/resources/testSrc/org/wickedsource/dependencytree/test/somepackage/InvalidDependencyTest.java
@@ -1,37 +1,13 @@
 //Don't import this
 //import org.wickedsource.dependencytree.test.somepackage.NotADependencyTest;
 
-//None of these typo imports should appear in the tree dependencies
-//Specified class does not exist         v
-import org.wickedsource.depencytree.test.CoreTes;
 
-//Don't improt this
-improt org.wickedsource.depencytree.test.somepackage.NotADependencyTest;
 
-//Specified package does not exist
-//         v
-import org.wicedsource.depencytree.test.CoreTest;
 
-//import specification can't end on a dot
-//                                         v
-import org.wickedsource.dependencytree.test.;
 
-//import specification can't start on a dot
-//     v
-import .org.wickedsource.dependencytree.test.somepackage.NotADependencyTest;
-
-//import specification can't end on an asterisk without having a dot before it
-//                                         v
-import org.wickedsource.dependencytree.test*;
-
-//import specification can't start on an asterisk
-//     v
-import *org.wickedsource.dependencytree.test;
 
 
 public class InvalidDependencyTest{
-    //Don't import this
-    import org.wickedsource.dependencytree.test.somepackage.NotADependencyTest;
 
     //Don't detect imports in strings
     String importBamboozleString = "import org.wickedsource.dependencytree.test.somepackage.NotADependencyTest;";
@@ -42,5 +18,3 @@ public class InvalidDependencyTest{
     }
 
 }
-//Don't import this
-import org.wickedsource.dependencytree.test.somepackage.NotADependencyTest;
